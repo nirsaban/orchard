0d1c9b01ae34cb91a8f8b546b370b9399ad7aa78
nirsa <nirsa11@gmail.com>
2020-02-02 16:53:42 +0200

    wip

--- app/Http/Controllers/GeneryController.php
+++ app/Http/Controllers/GeneryController.php
@@ -11,8 +11,8 @@ class GeneryController extends Controller
 {
     public function update(Request $request,$id){
 
-        $attr = json_decode($request);
-        return $request->item;
+        $attr = json_decode($request->item);
+        return $attr;
         $table = $attr->table;
         $col = $attr->col_name;
         $colVal= $attr->col_value;
