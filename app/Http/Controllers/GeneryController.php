c3f23ab000900d3b5cc3ceba9db2ee71ce6ec6c2
nirsa <nirsa11@gmail.com>
2020-02-02 18:57:13 +0200

    wip

--- app/Http/Controllers/GeneryController.php
+++ app/Http/Controllers/GeneryController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
 
 class GeneryController extends Controller
 {
-    public function update(Request $request,$id){
+    public static function update(Request $request,$id){
 
         $attr = json_decode($request->item);
 
@@ -23,6 +23,7 @@ class GeneryController extends Controller
         return response()->json('updated', 201);
     }
     public static  function delete(Request $request ,$id){
-                return $request->item;
+               $table =  $request->item;
+               $affected = DB::delete('DELETE FROM $table WHERE id = $id');
     }
 }
