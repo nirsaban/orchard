f3099caca40730516d03477ad94a8d155dbf27c5
nirsa <nirsa11@gmail.com>
2020-02-02 13:42:59 +0200

    wip

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -35,7 +35,7 @@ class ProjectController extends Controller
      */
     public function store(Request $request)
     {
-      return $request->project;
+     
      if(Project::saveNewProject($request)){
 
           return response()->json('Project Has Been Saved.', 201);
