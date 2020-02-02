bc339322406aa76ea8c2da2e12d0b4a213b4b5db
nirsa <nirsa11@gmail.com>
2020-02-02 13:40:54 +0200

    wip

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -36,13 +36,13 @@ class ProjectController extends Controller
     public function store(Request $request)
     {
       return $request->project;
-//     if(Project::saveNewProject($request)){
-//
-//          return response()->json('Project Has Been Saved.', 201);
-//     }
-//     else{
-//         return response()->json('There was a problem saving the project', 500);
-//     }
+     if(Project::saveNewProject($request)){
+
+          return response()->json('Project Has Been Saved.', 201);
+     }
+     else{
+         return response()->json('There was a problem saving the project', 500);
+     }
     }
 
     /**
