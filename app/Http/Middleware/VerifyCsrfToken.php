94732d72f31473450452e868639c12f0b737a11b
nirsa <nirsa11@gmail.com>
2020-02-02 03:14:04 +0200

    wip

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -21,8 +21,8 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         'http://606.co.il/*',
         'http://127.0.0.1:8000/*',
-        'http://localhost:8080',
-        'http://localhost:3000',
-        'http://localhost:3001',
+        'http://127.0.0.1:3000/*',
+        'http://127.0.0.1:3001/*',
+        'http://127.0.0.1:8000/*',
     ];
 }
