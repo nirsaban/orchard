9cc5f2e95f2899d90889b183bf6ba866e680843c
nirsa <nirsa11@gmail.com>
2020-02-02 18:42:01 +0200

    wip

--- app/Http/Controllers/GeneryController.php
+++ app/Http/Controllers/GeneryController.php
@@ -12,11 +12,11 @@ class GeneryController extends Controller
     public function update(Request $request,$id){
 
         $attr = json_decode($request->item);
-        return $attr->col_name;
+
         $tableName = $attr->table;
         $column = $attr->col_name;
         $colVal= $attr->col_value;
-        $affected = DB::update("update $tableName SET $column= $colVal where id = $id");
+        $affected = DB::update("UPDATE $tableName SET $column= '$colVal' WHERE id = $id");
 //        $element = $request->table::findOrFail($id);
 //        $element->update([$request->col_name => $request->col_value]);
 
