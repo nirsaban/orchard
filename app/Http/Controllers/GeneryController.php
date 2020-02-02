5ed2313fcb4d60e5fd4f6779ad6c2917bc652b59
nirsa <nirsa11@gmail.com>
2020-02-02 16:49:05 +0200

    wip

--- app/Http/Controllers/GeneryController.php
+++ app/Http/Controllers/GeneryController.php
@@ -10,14 +10,16 @@ use Illuminate\Support\Facades\DB;
 class GeneryController extends Controller
 {
     public function update(Request $request,$id){
+
         $attr = json_decode($request);
+        return $attr;
         $table = $attr->table;
         $col = $attr->col_name;
         $colVal= $attr->col_value;
         $affected = DB::update("update '$table' set '$col' = '$colVal' where id = ? ['$id']");
 //        $element = $request->table::findOrFail($id);
 //        $element->update([$request->col_name => $request->col_value]);
-        
+
         return response()->json('updated', 201);
     }
 }
