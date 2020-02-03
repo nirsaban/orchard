8dedae1a8d14059660ff94a5b1fb4c5f4c1f2b56
nirsa <nirsa11@gmail.com>
2020-02-03 22:07:46 +0200

    wip

--- app/Http/Controllers/GeneryController.php
+++ app/Http/Controllers/GeneryController.php
@@ -23,9 +23,9 @@ class GeneryController extends Controller
     }
     public static  function delete(Request $request ,$id){
                $table =  $request->item;
-                DB::table($table)->where('id','=' ,'$id')->delete();
-
-//               $affected = DB::delete("DELETE FROM $table WHERE id = $id");
-              return response()->json('deleted', 201);
+              $affected =  DB::table($table)->where('id','=' ,'$id')->delete();
+             return $affected;
+////               $affected = DB::delete("DELETE FROM $table WHERE id = $id");
+//              return response()->json('deleted', 201);
     }
 }
