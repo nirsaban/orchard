356ce6dcb8c8258f49750662f9c7be6aa364d9a3
nirsa <nirsa11@gmail.com>
2020-02-03 21:33:22 +0200

    wip

--- app/Http/Controllers/GeneryController.php
+++ app/Http/Controllers/GeneryController.php
@@ -10,17 +10,20 @@ use Illuminate\Support\Facades\DB;
 class GeneryController extends Controller
 {
     public static function update(Request $request,$id){
-       
+
         $attr = json_decode($request->item);
         $tableName = $attr->table;
         $column = $attr->col_name;
         $colVal= $attr->col_value;
-
-        $affected = DB::update("UPDATE $tableName SET $column= '$colVal' WHERE id = $id");
+        $query = DB::table($tableName)
+            ->where('id', $id)
+            ->update(array($column => $colVal));
+//        $affected = DB::update("UPDATE $tableName SET $column= '$colVal' WHERE id = $id");
         return response()->json('Updated Successfully', 201);
     }
     public static  function delete(Request $request ,$id){
                $table =  $request->item;
+
                $affected = DB::delete("DELETE FROM $table WHERE id = $id");
               return response()->json('deleted', 201);
     }
