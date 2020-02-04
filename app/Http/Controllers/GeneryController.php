65de095abe621ed8f4639ea43895bb88b96f86d8
nirsa <nirsa11@gmail.com>
2020-02-04 10:25:38 +0200

    wip

--- app/Http/Controllers/GeneryController.php
+++ app/Http/Controllers/GeneryController.php
@@ -15,17 +15,28 @@ class GeneryController extends Controller
         $tableName = $attr->table;
         $column = $attr->col_name;
         $colVal= $attr->col_value;
-        $query = DB::table($tableName)
+        $affected = DB::table($tableName)
             ->where('id', $id)
             ->update(array($column => $colVal));
 //        $affected = DB::update("UPDATE $tableName SET $column= '$colVal' WHERE id = $id");
+        if($affected > 0){
+            return response()->json('deleted', 201);
+
+        }else{
+            return response()->json('something wrong', 201);
+        }
         return response()->json('Updated Successfully', 201);
     }
     public static  function delete(Request $request ,$id){
                $table =  $request->item;
-              $affected =  DB::table('$table')->where('id','=' ,'$id')->delete();
-             return $affected;
+              $affected =  DB::table($table)->where('id','=' ,$id)->delete();
+
 ////               $affected = DB::delete("DELETE FROM $table WHERE id = $id");
-              return response()->json('deleted', 201);
+             if($affected > 0){
+                 return response()->json('deleted', 201);
+
+             }else{
+                 return response()->json('something wrong', 201);
+             }
     }
 }
