39ac39673742176cc2d4a1e4dba1a17c18f0133c
nirsa <nirsa11@gmail.com>
2020-02-04 21:24:37 +0200

    wip

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -19,7 +19,7 @@ public function save(Request $request){
 //            $color = $product->color;
 //           $attr = DB::select("SELECT sku from products WHERE products_name = '$product_name' and type = '$type' and shingle_type = '$shingle_type' and color = '$color'");
 //           return $attr;
-          $attr = DB::select("$sql")->get();
+          $attr = DB::select("$sql");
           $sku = $attr[0];
          print_r($sku['sku']);
 
