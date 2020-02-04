25443ed666da1e1e01ae8bd581d8c2ef1a0fb2a8
nirsa <nirsa11@gmail.com>
2020-02-04 18:03:22 +0200

    wip

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class ProductsController extends Controller
 {
 public function save(Request $request){
-    return $request->products;
+    return $request->product;
 }
 
 }
