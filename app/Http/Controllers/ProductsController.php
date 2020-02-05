22cefa47a49cbc91ccd1af8e1ee1314e7f6e5428
nirsa <nirsa11@gmail.com>
2020-02-05 15:35:10 +0200

    wip

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -32,9 +32,7 @@ public function save(Request $request)
          public function showOrder(Request $request){
       $id  = $request->id;
        $affected = Order::select('sku')->where('project_id',$id);
-       $affected->toArray();
-
-        return $affected;
+        return $affected->get();
         }
 
 }
