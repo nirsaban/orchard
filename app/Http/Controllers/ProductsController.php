a9204e88f1bbb43e9e8d2b2564a2df1e602fff2c
nirsa <nirsa11@gmail.com>
2020-02-06 16:05:20 +0200

    wip

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -50,7 +50,7 @@ public  static  function save(Request $request)
       }
          public  function Update(Request $request,$id){
 
-             DB::table('orders')->delete($id);
+             DB::delete("delete from orders where id = '$id'");
 
                 }
 
