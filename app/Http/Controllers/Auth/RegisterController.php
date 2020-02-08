1e195523deef0065c21c8f7859176d9f97d0f738
nirsa <nirsa11@gmail.com>
2020-02-08 19:44:31 +0200

    wip

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,7 +52,7 @@ class RegisterController extends Controller
 
     {
         return $request->user;
-        return $data;
+    
 //        return Validator::make($data, [
 //            'name' => ['required', 'string', 'max:255'],
 //            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
