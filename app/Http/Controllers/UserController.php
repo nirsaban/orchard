8ab8ed9243d10ea9c7cc261782d33a5051dd1a43
nirsa <nirsa11@gmail.com>
2020-02-09 15:34:09 +0200

    wip

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,6 @@ class UserController extends Controller
 {
     public static function validateUser(Request $request){
             $attr = json_decode($request->user);
-          return $attr->email;
+          return bcrypt($attr->password);
     }
 }
