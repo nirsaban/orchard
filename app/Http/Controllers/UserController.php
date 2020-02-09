a515344e6b253180fcf4d13c048bcd92876ab329
nirsa <nirsa11@gmail.com>
2020-02-09 16:19:49 +0200

    wip

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,14 +5,15 @@ use App\Http\Requests\loginRequest;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-
+use Illuminate\Support\Facades\Session;
 class UserController extends Controller
 {
     public static function validateUser(Request $request){
           $attr = json_decode($request->user);
          if ($user = User::where('email',$attr->email)->first()->toArray()){
              if(Hash::check($attr->password,$user['password'])){
-                 echo "all Good";
+                  return $session = [Session::put('user_id',$user['id']),Session::put('user_name',$user['name'])];
+
              }
          }
     }
