f6de3876420681bc497cca55fdeda77e9181930e
nirsa <nirsa11@gmail.com>
2020-02-09 18:07:07 +0200

    wip

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -15,19 +15,18 @@ class ProductsController extends Controller
     }
 public   function save(Request $request)
 {
-
-
-    $product = json_decode($request->product);
-    foreach ($product as $key => $value) {
-        $attr = Product::select('sku')->where($key,$value);
-    }
-    $res = $attr->get();
-    $sku= $res[0]->sku;
-    $insert =  new Order;
-    $insert -> project_id = 2;
-    $insert ->user_id = 10;
-    $insert -> sku = $sku;
-    $insert ->save();
+    echo __METHOD____;
+//    $product = json_decode($request->product);
+//    foreach ($product as $key => $value) {
+//        $attr = Product::select('sku')->where($key,$value);
+//    }
+//    $res = $attr->get();
+//    $sku= $res[0]->sku;
+//    $insert =  new Order;
+//    $insert -> project_id = 2;
+//    $insert ->user_id = 10;
+//    $insert -> sku = $sku;
+//    $insert ->save();
          }
       public static  function edit(Request $request,$sku){
             ///get all option to this column of specific products
